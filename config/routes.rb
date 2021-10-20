0ee14b2b8c03da92e18e76216ef86f8b61c59530
Renan Porto <renan.porto1099@gmail.com>
2021-10-20 13:50:29 -0300

    generate scaffold contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # cria automaticamente os padrões REST, show, index, etc
+  resources :contacts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "static_pages#index"
