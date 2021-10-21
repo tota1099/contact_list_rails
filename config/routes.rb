9dfe77d980e22f68a896e57bdcaab2a6f8dd2be6
Renan Porto <renan.porto1099@gmail.com>
2021-10-20 21:36:58 -0300

    criando controller users e adicionando validacoes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # cria automaticamente os padrões REST, show, index, etc
   resources :contacts
+  resources :users, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "static_pages#index"
